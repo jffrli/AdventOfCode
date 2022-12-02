5d7a5ee67eb84f8c754f196e06cf137c21086239
jffrli <jffr.li1@gmail.com>
2022-12-01 22:20:59 -0800

    Added 2022, day 1

---
+++ 2022/day1/solution.py
@@ -0,0 +1,12 @@
+with open("input", "r") as f:
+    cals = f.read()
+elves = cals.split("\n\n")
+#print(len(elves))
+ecs = []
+for elf in elves:
+    ec = [int(x) for x in elf.split("\n")]
+    ecs.append(sum(ec))
+
+list.sort(ecs)
+print(ecs[-1]) #part 1
+print(sum(ecs[-3:])) #part 2
