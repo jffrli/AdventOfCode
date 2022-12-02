6a7017a0187aec18d9f2d3b6cdd07628a9f970ce
jffrli <jffr.li1@gmail.com>
2022-12-01 22:22:24 -0800

    Added 2022, day 2

---
+++ 2022/day2/part1.py
@@ -0,0 +1,29 @@
+with open("input","r") as f:
+    lines = f.readlines()
+
+plays = [x.split() for x in lines]
+
+
+score = 0
+
+for o,s in plays:
+    if s == "X":
+        score += 1
+        if o == "A":
+            score += 3
+        elif o == "C":
+            score += 6
+    elif s == "Y":
+        score += 2
+        if o == "B":
+            score += 3
+        elif o == "A":
+            score += 6
+    elif s == "Z":
+        score += 3
+        if o == "C":
+            score += 3
+        elif o == "B":
+            score += 6
+
+print(score)
---
+++ 2022/day2/part2.py
@@ -0,0 +1,34 @@
+with open("input","r") as f:
+    lines = f.readlines()
+
+plays = [x.split() for x in lines]
+
+
+score = 0
+
+for o,s in plays:
+    if s == "X":
+        if o == "A":
+            score += 3
+        elif o == "B":
+            score += 1
+        elif o == "C":
+            score += 2
+    elif s == "Y":
+        score += 3
+        if o == "A":
+            score += 1
+        elif o == "B":
+            score += 2
+        elif o == "C":
+            score += 3
+    elif s == "Z":
+        score += 6
+        if o == "A":
+            score += 2
+        elif o == "B":
+            score += 3
+        elif o == "C":
+            score += 1
+
+print(score)
