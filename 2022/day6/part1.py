54a957f1404caf9f1addd6105b4643fb72d9de7b
jffrli <jffr.li1@gmail.com>
2022-12-05 21:13:19 -0800

    Finished 2022 day 6

---
+++ 2022/day6/part1.py
@@ -0,0 +1,12 @@
+with open("input.txt","r") as f:
+	message = f.read()
+
+start = message[:3]
+
+for i in range(3,len(message)):
+	x = message[i]
+	if len(set(start)) == 3 and x not in start:
+		print(i+1)
+		break
+	start = start[1:]
+	start += x
---
+++ 2022/day6/part2.py
@@ -0,0 +1,12 @@
+with open("input.txt","r") as f:
+	message = f.read()
+
+start = message[:13]
+
+for i in range(3,len(message)):
+	x = message[i]
+	if len(set(start)) == 13 and x not in start:
+		print(i+1)
+		break
+	start = start[1:]
+	start += x
