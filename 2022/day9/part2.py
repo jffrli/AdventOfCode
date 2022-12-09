3127ccaa9113e64e632647b106adf9506b5943c1
Jeffrey Li <jli@operto.com>
2022-12-09 13:47:34 -0800

    Completed 2022, day 9

---
+++ 2022/day9/part2.py
@@ -0,0 +1,53 @@
+def follow(head,tail):
+	if abs(head[1]-tail[1]) > 1 and head[0] == tail[0]:
+		if head[1] > tail[1]:
+			tail[1] += 1
+		else:
+			tail[1] -= 1
+	elif abs(head[0]-tail[0]) > 1 and head[1] == tail[1]:
+		if head[0] > tail[0]:
+			tail[0] += 1
+		else:
+			tail[0] -= 1
+	elif abs(head[1]-tail[1]) > 1 or abs(head[0]-tail[0]) > 1: # move diagonal
+		if head[0] > tail[0]:
+			tail[0] += 1
+		else:
+			tail[0] -= 1
+		if head[1] > tail[1]:
+			tail[1] += 1
+		else:
+			tail[1] -= 1
+
+with open("input.txt", "r") as f:
+	data = [x.strip().split() for x in f.readlines()]
+
+visited = set()
+
+knots = [[0,0] for i in range(10)]
+
+for action in data:
+	di = action[0]
+	if di == "R":
+		dx = 1
+		dy = 0
+	elif di == "L":
+		dx = -1
+		dy =0 
+	elif di == "U":
+		dy = 1
+		dx = 0
+	else:
+		dy = -1
+		dx = 0
+
+	for i in range(int(action[1])):
+
+		knots[0][0] += dx
+		knots[0][1] += dy
+		
+		for j in range(1,len(knots)):
+			follow(knots[j-1],knots[j])
+		visited.add(tuple(knots[-1]))
+
+print(len(visited))
