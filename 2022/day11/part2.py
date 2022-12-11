0d78b7fef757f43e53d5a88ab7af22bebba0b158
jffrli <jffr.li1@gmail.com>
2022-12-11 00:45:39 -0800

    Completed 2022, day 11

---
+++ 2022/day11/part2.py
@@ -0,0 +1,62 @@
+def findlcm(monkeys):
+	vals = [int(x[-3].split()[-1]) for x in monkeys]
+	from math import lcm
+	return lcm(*vals)
+
+def inspection(monkey, num, inventories, inspects, lcm):
+	operation = monkey[2].split("=")[1].split()
+	
+	test = int(monkey[-3].split()[-1])
+
+	#print(monkey[-1].split())
+	tm = int(monkey[-2].split()[-1])
+	fm = int(monkey[-1].split()[-1])
+
+	if operation[1] == "*":
+		if  operation[2] == "old":
+			op = lambda x : x * x
+		else:
+			op = lambda x : x * int(operation[2])
+	else:
+		if  operation[2] == "old":
+			op = lambda x : x + x
+		else:
+			op = lambda x : x + int(operation[2])
+
+	while len(inventories[num]):
+		item = inventories[num].pop()
+		inspects[num] += 1
+
+		newitem = op(item)%lcm
+
+		if not newitem%test:
+			inventories[tm].append(newitem)
+		else:
+			inventories[fm].append(newitem)
+
+
+with open("input.txt", "r") as f:
+	data = f.read().split("\n\n")
+
+monkeys = [[y for y in x.split("\n") if y] for x in data]
+
+inventories = [[] for x in range(len(monkeys))]
+inspects = [0 for x in range(len(monkeys))]
+
+lcm = findlcm(monkeys)
+
+#initiate
+for i in range(len(monkeys)):
+	monkey = monkeys[i]
+	items = monkey[1].split(":")[1].split(", ")
+	inventories[i] = [int(x) for x in items]
+
+#10000 rounds
+for r in range(10000):
+	#print("Round: ",r)
+	for i in range(len(monkeys)):
+		#print(inventories[i])
+		inspection(monkeys[i],i,inventories,inspects, lcm)
+
+inspects.sort()
+print(inspects[-1]*inspects[-2])
