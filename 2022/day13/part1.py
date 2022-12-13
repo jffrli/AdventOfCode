8aa975e04620684526964849ee9fecdf79a6e86d
jffrli <jffr.li1@gmail.com>
2022-12-12 21:42:57 -0800

    Starting off 2022, day 13.

---
+++ 2022/day13/part1.py
@@ -0,0 +1,25 @@
+def compare(left, right):
+	if isinstance(left,list) or isinstance(right,list):
+		if not isinstance(left,list):
+			left = [left]
+		if not isinstance(right,list):
+			right = [right]
+		for i in range(len(left)):
+			if i >= len(right):
+				return False
+			if not compare(left[i],right[i]):
+				return False
+		return True
+	else:
+		return left <= right
+
+with open("input.txt", "r") as f:
+	data = [eval(x.strip()) for x in f.readlines() if x.strip()]
+
+total = 0
+for i in range(0,len(data),2):
+
+	if not compare(data[i], data[i+1]):
+		total += 1 + int(i/2)
+
+print(total)
